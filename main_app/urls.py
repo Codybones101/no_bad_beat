68e0d4f002cdf77a56b17b1eb0bb0359708f82d1
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2023-03-06 10:56:32 -0800

    bets_index made

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-
-
+    path('bets/', views.bets_index, name="bets_index"),
     path('accounts/signup/', views.signup, name='signup'),
 ]
