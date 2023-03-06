605e23c789c45ddb0417cd260bad25550415e1a3
richardmundyiii <mundy.richard@icloud.com>
2023-03-06 15:05:00 -0800

    connect api

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    game_id = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    home_team = models.CharField(max_length=100)
+    last_update = models.CharField(max_length=100)
+    away_price = models.IntegerField()
+    home_price = models.IntegerField()
