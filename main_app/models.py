e7ef25eaba92a4ec69c9036897a84a7967347967
richardmundyiii <mundy.richard@icloud.com>
2023-03-07 15:14:01 -0800

    Update auto api

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     game_id = models.CharField(max_length=100)
+    game_date = models.DateField(blank=True)
     away_team = models.CharField(max_length=100)
     home_team = models.CharField(max_length=100)
     away_price = models.IntegerField()
