6aa9d391dff46baba2984d2cf9ca572eb1caa597
richardmundyiii <mundy.richard@icloud.com>
2023-03-08 22:08:30 -0800

    betting logic

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Comment
+from .models import Comment, Bet
 
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['comment']
+
+class BetForm(ModelForm):
+    class Meta:
+        model = Bet
+        fields = ['bet_size']
--- main_app/models.py
+++ main_app/models.py
@@ -17,3 +17,11 @@ class Comment(models.Model):
     comment_date = models.DateField(auto_now_add=True, blank=True)
     comment = models.TextField(max_length=250)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+
+class Bet(models.Model):
+    bet_user = models.CharField(max_length=100)
+    team_picked = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    bet_size = models.IntegerField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
